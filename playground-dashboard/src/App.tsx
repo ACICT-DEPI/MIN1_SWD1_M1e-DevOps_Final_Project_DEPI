1451d61b03d3fa7743e4570183bdcbc7aa16bd38
ali ismail <aliismailh08@gmail.com>
2024-10-18 22:36:33 +0300

    v1.0.0

---
+++ playground-dashboard/src/App.tsx
@@ -0,0 +1,31 @@
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import { routers } from "./route";
+import { OwnersProvider } from "./context/ownersContext";
+import { Toaster } from "react-hot-toast";
+import { AdminProvider } from "./context/adminContext";
+import { PlayersProvider } from "./context/playersContext";
+import { FeedbackProvider } from "./context/FeedbackContext";
+
+
+const Router = createBrowserRouter(routers)
+
+function App() {
+
+  return (
+    <>
+     <AdminProvider>
+     <OwnersProvider>
+     <PlayersProvider>
+      <FeedbackProvider>
+      <Toaster/>
+      <RouterProvider router={Router} />
+      </FeedbackProvider>
+        </PlayersProvider>
+      </OwnersProvider>
+     </AdminProvider>
+     
+    </>
+  )
+}
+
+export default App
